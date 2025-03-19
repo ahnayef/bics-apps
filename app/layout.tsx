084901c59a80e0047fc57ae769e2d514f9ef264b
AHNayef <ahnayef@outlook.com>
2025-03-19 16:50:40 +0600

    Enhance metadata in layout with authors, keywords, and Open Graph settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,49 @@ const notoSansBengali  = Noto_Sans_Bengali({
   subsets: ["bengali"]
 });
 
+
 export const metadata: Metadata = {
- title: 'অ্যাপস',
-  description: 'সহজেই ডাউনলোড করুন নবীন, তরুণ এবং অগ্রদূত '
-};
+  title: 'অ্যাপস',
+  description: 'সহজেই ডাউনলোড করুন নবীন, তরুণ এবং অগ্রদূত',
+  authors: [{ name: "AHNayef", url: "https://github.com/ahnayef" }],
+  keywords: ["education", "career"],
+  metadataBase: new URL("https://bics-apps.vercel.app"),
+  themeColor: "#4b1535",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    userScalable: true,
+  },
+
+
+  openGraph: {
+    url: 'https://bics-apps.vercel.app',
+    siteName: 'bics-apps',
+    images: [
+      {
+        url: 'meta.png',
+        width: 1280,
+        height: 720,
+        alt: 'Meta Image',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    creator: "bics-apps",
+    site: "https://bics-apps.vercel.app",
+    images: [
+      {
+        url: 'meta.png',
+        width: 1280,
+        height: 720,
+        alt: 'Meta Image',
+      },
+    ],
+  }
+
+}
 
 export default function RootLayout({
   children,
